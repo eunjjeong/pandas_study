bf8b2e4378f8ae7d4c6635b8bb31d2a67bb547f4
jjeong <(bigi5@naver.com)>
2020-01-06 12:47:24 +0900

    200106 pandas study

---
+++ groupby_test/df_groupby_filter.py
@@ -0,0 +1,14 @@
+import seaborn as sns
+
+titanic = sns.load_dataset('titanic')
+df = titanic.loc[:, ['age', 'sex', 'class', 'fare', 'survived']]
+
+grouped = df.groupby(['class'])
+
+print("# 데이터 개수가 200개 이상인 그룹만을 필터링하여 데이터프레임으로 반환")
+grouped_filter = grouped.filter(lambda x: len(x) >= 200)
+print(grouped_filter.head(), '\n', type(grouped_filter))
+print()
+print("# age 열의 평균이 30보다 작은 그룹만을 필터링하여 데이터프레임으로 반환")
+age_filter = grouped.filter(lambda x: x.age.mean() < 30)
+print(age_filter.tail(), '\n', type(age_filter))
